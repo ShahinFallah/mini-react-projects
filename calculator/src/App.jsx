080be029aa430fe00ba277ab99aec2c72c31bb35
ShahinFallah <shahinfallah2006@gmail.com>
2024-03-20 18:37:11 +0330

    add Html and give Style to calculator

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,5 +1,28 @@
 export default function App() {
   return (
-    <>Hello</>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="prev-operand">123,234  123 141 341 4 *</div>
+        <div className="current-operand">555</div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>×</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
+    </div>
   )
 }
